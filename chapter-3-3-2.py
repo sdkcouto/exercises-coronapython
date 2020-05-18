973b716544431914e73dc0f4296d1248f61edfe1
sdkcouto <matheuscouto.eng@gmail.com>
2020-05-17 23:00:54 -0300

    More Activities

--- chapter-3-3-2.py
+++ chapter-3-3-2.py
@@ -1,7 +1,6 @@
 # 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
 
-names = ["nome1", "nome2", "nome3"]
-
-print("Hi " + names[0] + " you are the best!")
-print("Hi " + names[1] + " you are the best!")
-print("Hi " + names[2] + " you are the best!")
+names=[" Matheus ", " Dudu ", " Andre "]
+print("Hello"+names[0]+"how are you?")
+print("Hello"+names[1]+"how are you?")
+print("Hello"+names[2]+"how are you?")
--- chapter-3-3-4.py
+++ chapter-3-3-4.py
@@ -1,7 +1,6 @@
 # 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
-presidents = ["Obama", "Lula", "Merkel"]
-
-print("Hey, " + presidents[0] + " i am making dinner, please come!")
-print("Hey, " + presidents[1] + " i am making dinner, please come!")
-print("Hey, " + presidents[2] + " i am making dinner, please come!")
+presidents = ["Lula", "Bolsonaro", "Putin"]
+print("hey, "+ presidents[0] + " would you like to have dinner?")
+print("hey, "+ presidents[1] + " would you like to have dinner?")
+print("hey, "+ presidents[2] + " would you like to have dinner?")
